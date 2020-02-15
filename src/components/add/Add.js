b69b482ccb2abc6a7ac1aa5d37998062ef3f128b
rider <riderjensen@gmail.com>
2020-02-15 14:37:47 -0700

    adding card and plus

---
+++ src/components/add/Add.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function AddCard() {
+    return (
+        <div>
+            <FontAwesomeIcon icon={faPlusCircle} />
+        </div>
+    )
+}
