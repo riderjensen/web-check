98f033a339b5d2bd330dbb3c5251e68e21f9f6a4
rider <riderjensen@gmail.com>
2020-02-15 14:36:17 -0700

    Changing state naming conventions

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,13 @@
 import * as actionTypes from '../actionTypes';
 
 const initialState = {
-	items: []
+	trackers: []
   }
   
 export default function reducer(state = initialState, action) {
 	switch (action.type) {
 		case actionTypes.ADD_NEW_TRACKER:
-			return {...state, items: action.payload}
+			return {...state, trackers: action.payload}
 		default:
 			return state;
 	}
