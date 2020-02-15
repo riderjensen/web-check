d20d80a135e0e6746e98380623ff1d3bd38a4992
rider <riderjensen@gmail.com>
2020-02-15 16:06:50 -0700

    basic card info placeholder and delete add

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -5,7 +5,9 @@ import './Card.css'
 export default function Card (props) {
     return (
         <div className="card">
-            <p>Card</p>
+                <p className="title">{props.title}</p>
+                <p>Refresh time: {props.timeout}</p>
+                <a href={props.url} target="_blank" rel="noopener noreferrer">Visit</a>
         </div>
     )
 }
