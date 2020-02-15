af10e737f8b7220e075495a64446bc2dc242a813
rider <riderjensen@gmail.com>
2020-02-15 14:38:07 -0700

    pulling in props and minor css changes

--- src/containers/home-container/HomeContainer.js
+++ src/containers/home-container/HomeContainer.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import AddCard from '../../components/add/Add';
+import Card from '../../components/cards/Card';
 
 import './HomeContainer.css';
-
 
 class Home extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Home</h1>
+            <div className="home-container">
+                {this.props.trackers.map(track => {
+                    return <Card {...track} />
+                })}
+                <AddCard />
             </div>
         )
     }
 }
 
-export default connect()(Home);
+const mapStateToProps = state => {
+    return {
+        trackers: state.index.trackers
+    }
+}
+
+export default connect(mapStateToProps, null)(Home);
