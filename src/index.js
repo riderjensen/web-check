2fa8d01a5a686d4f06eabca6b0ffec5e22ea8c08
rider <riderjensen@gmail.com>
2020-02-15 14:06:32 -0700

    Include global state to application

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './store/reducers/index';
+
+const rootReducer = combineReducers({
+    index: reducers,
+    // other reducers as they are created
+})
+
+const reduxComps = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) || compose;
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), reduxComps));
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
