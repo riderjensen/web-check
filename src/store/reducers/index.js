7ba4044c26d7c879c904fd54a8a92e887ae57cef
rider <riderjensen@gmail.com>
2020-02-15 16:07:19 -0700

    fake tracking info added

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,26 @@
 import * as actionTypes from '../actionTypes';
 
 const initialState = {
-	trackers: []
+	trackers: [
+		{
+			id: 1,
+			title: "My title",
+			url: 'https://google.com',
+			timeout: 10,
+			indicators: [
+
+			]
+		},
+		{
+			id: 2,
+			title: "BestBuy Switch",
+			url: 'https://google.com',
+			timeout: 10,
+			indicators: [
+				
+			]
+		},
+	]
   }
   
 export default function reducer(state = initialState, action) {
